af3a78742a63a8c972a27b8d967654db5cba6061
dqmdz <daniel.quinterospinto@gmail.com>
2026-03-31 04:39:59 -0300

    feat(consumo): add AllArgsConstructor and NoArgsConstructor to ConsumoContextDto
    Added Lombok annotations to improve JSON serialization support.
    
    Also bumped version to 1.0.1 and updated CHANGELOG.

--- src/main/java/sauce/agua/rest/model/internal/ConsumoContextDto.java
+++ src/main/java/sauce/agua/rest/model/internal/ConsumoContextDto.java
@@ -1,13 +1,17 @@
 package sauce.agua.rest.model.internal;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConsumoContextDto {
 
     private Long clienteId;
